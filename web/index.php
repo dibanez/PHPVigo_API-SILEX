b642b71555989d44a1732b249eda1d471c6aa35c
David Ibáñez <dibanez@gmail.com>
2016-12-02 10:07:34 +0100

    Tema 3

--- web/index.php
+++ web/index.php
@@ -1,10 +1,30 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php'; 
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-$app = new Silex\Application(); 
+$app = new Silex\Application();
 
-$app->get('/hola/{name}', function($name) use($app) { 
-    return 'Hola '.$app->escape($name); 
-}); 
+$app['driver'] = 'pdo_sqlite';
+$app['path'] = __DIR__.'/../db/datos.db';
 
-$app->run(); 
+use Silex\Provider\DoctrineServiceProvider;
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => $app['driver'],
+        'path' => $app['path']
+    ),
+));
+
+
+$app->get('/hola/{name}', function($name) use ($app) {
+    return 'Hola '.$app->escape($name);
+});
+
+$app->get('/usuarios/', function() use ($app) {
+    $sql = "select * from usuarios";
+    $datos = $app['db']->fetchAll($sql);
+    return new JsonResponse( $datos, 200 );
+});
+
+
+$app->run();
